8bfaa66721853477ce6dfe3bd3354149928a2364
luzhaigo <gitluzhai@gmail.com>
2019-04-18 01:05:48 +0800

    update mongoose version and update the mongodb uri

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ require('./models/Blog');
 require('./services/passport');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { useMongoClient: true });
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true
+})
 
 const app = express();
 
